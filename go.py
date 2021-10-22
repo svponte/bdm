8d9fce0f3259a604e283f94aaf291d2b5565efee
Samuel Ponte <svponte@gmail.com>
2021-10-22 08:41:18 -0300

    Scripts de COPY

---
+++ go.py
@@ -0,0 +1,43 @@
+
+
+# 35.203.92.156
+################ Biblioteca
+from time import time
+from cassandra.cluster import Cluster
+from cassandra.query import tuple_factory
+import pandas as pd
+
+cluster = Cluster(['35.203.92.156'])
+session = cluster.connect()
+formato = 'utf8' 
+
+# Contador de tempo
+def tic():
+    global _start_time 
+    _start_time = time()
+
+def tac():
+    t_sec = round(time.time() - _start_time)
+    (t_min, t_sec) = divmod(t_sec,60)
+    (t_hour,t_min) = divmod(t_min,60) 
+    print('Duração: {}hour:{}min:{}sec'.format(t_hour,t_min,t_sec))
+
+print ('# # Início # #')
+print(str(time() ))
+################ Cria Keyspace
+KEYSPACE = "microdados"
+session.set_keyspace(KEYSPACE)
+
+#tic()
+session = cluster.connect(KEYSPACE)
+session.row_factory = tuple_factory
+#rows = session.execute("SELECT CO_MUNICIPIO_PROVA, NU_ANO FROM enem WHERE CO_MUNICIPIO_PROVA = 3109006;")
+rows = session.execute("SELECT * FROM enem WHERE CO_MUNICIPIO_PROVA = 3109006;")
+#rows = session.execute("SELECT count(*) FROM enem ;")
+print (rows[1])
+
+session.shutdown()
+print ('# # FIM # #')
+print(str(time() ))
+
+#tac()
