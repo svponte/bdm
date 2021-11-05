205427a8b68fcf24e5cead84bd0a76711a44295c
Leothi <leohb2@gmail.com>
2021-11-05 10:36:59 -0300

    autopep8

--- go.py
+++ go.py
@@ -5,7 +5,6 @@
 from time import time
 from cassandra.cluster import Cluster
 from cassandra.query import tuple_factory
-import pandas as pd
 
 cluster = Cluster(['35.203.92.156'])
 session = cluster.connect()
@@ -26,9 +25,9 @@ def tac():
     print('Duração: {}hour:{}min:{}sec'.format(t_hour, t_min, t_sec))
 
 
-print ('# # Início # #')
+print('# # Início # #')
 #print(str(time() ))
-################ Cria Keyspace
+# Cria Keyspace
 KEYSPACE = "bdm"
 #KEYSPACE = "microdados"
 session.set_keyspace(KEYSPACE)
@@ -37,16 +36,17 @@ session.set_keyspace(KEYSPACE)
 session = cluster.connect(KEYSPACE)
 session.row_factory = tuple_factory
 #rows = session.execute("SELECT IN_AMPLIADA_24, ST_CONCLUSAO FROM ENEM WHERE TP_LINGUA = 1;")
-rows = session.execute("SELECT IN_AMPLIADA_24, ST_CONCLUSAO FROM ENEM WHERE nu_inscrical > 190000000000 AND NU_ANO = 2019 and tp_sexo = 1 limit 1;")
+rows = session.execute(
+    "SELECT IN_AMPLIADA_24, ST_CONCLUSAO FROM ENEM WHERE nu_inscrical > 190000000000 AND NU_ANO = 2019 and tp_sexo = 1 limit 1;")
 #rows = session.execute("alter table <table_name> with GC_GRACE_SECONDS = <timeout>;")
 #rows = session.execute("alter table enem with GC_GRACE_SECONDS = 3600;")
 
 #rows = session.execute("UPDATE COLUMN FAMILY cf with GC_GRACE = 86400;")
 
-print (rows[0])
+print(rows[0])
 
 session.shutdown()
-print ('# # FIM # #')
+print('# # FIM # #')
 #print(str(time() ))
 
 # tac()
